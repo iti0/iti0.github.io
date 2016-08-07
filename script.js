bf161342d2e6db1b1d30ba6a6111aefe5adad059
BuildTools <unconfigured@null.spigotmc.org>
2016-08-07 13:57:13 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 m = [
-		setInfo("c1",12,29,5,"black",3),
-		setInfo("c1",11,28,5,"black",3),
-		setInfo("c1",12,28,5,"black",3),
-		setInfo("c1",13,27,5,"black",3),
-		setInfo("c1",13,25,5,"black",3),
-		setInfo("c1",13,26,5,"black",3),
-		setInfo("c1",12,26,5,"black",3),
-		setInfo("c1",13,28,5,"black",3),		
-		setInfo("c1",10,40,5,"black",3),		
-		setInfo("c1",50,50,5,"black",3)		]
+		setInfo("k15",12,29,5,"black",3),
+		setInfo("k15",11,28,5,"black",3),
+		setInfo("k15",12,28,5,"black",3),
+		setInfo("k15",13,27,5,"black",3),
+		setInfo("k15",13,25,5,"black",3),
+		setInfo("k15",13,26,5,"black",3),
+		setInfo("k9",12,26,5,"black",3),
+		setInfo("k9",13,28,5,"black",3),		
+		setInfo("k9",10,40,5,"black",3),		
+		setInfo("k9",50,50,5,"black",3)		]
 function setInfo(sclass, cx, cy, r, fill,xzoom){
 	cxz = cx*900/100
 	cyz = cy*900/100
 	return {
-		"class":sclass,
+		"xclass":sclass,
 		"cx":cxz.toString(),
 		"cy":cyz.toString(),
 		"r":r.toString(),
@@ -23,7 +23,7 @@ function setInfo(sclass, cx, cy, r, fill,xzoom){
 }
 function setCircle(options){
 	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c.className = options.class;
+	c.setAttribute("class", options.xclass)
 	c.setAttribute("cx", options.cx)
 	c.setAttribute("cy", options.cy)
 	c.setAttribute("r", options.r)
